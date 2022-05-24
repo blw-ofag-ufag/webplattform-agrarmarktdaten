6cd317ac73497eae49a0a7213dde4342ed3ee2f7
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2022-05-24 12:22:26 +0200

    feat: Allow ContentContainer to be narrow

--- src/components/content-container.tsx
+++ src/components/content-container.tsx
@@ -2,11 +2,17 @@ import { Box, SxProps } from "@mui/material";
 import React from "react";
 
 export const ContentContainer = ({
+  narrow = false,
   children,
   sx,
 }: {
   children: React.ReactNode;
+  narrow?: boolean;
   sx?: SxProps;
 }) => {
-  return <Box sx={{ ...sx, maxWidth: "77rem", mx: "auto" }}>{children}</Box>;
+  return (
+    <Box sx={{ ...sx, maxWidth: narrow ? "60rem" : "77rem", mx: "auto" }}>
+      {children}
+    </Box>
+  );
 };
