c12f0d47c37e3b02a8b9fa967736c0ab6de1af0f
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2022-05-31 11:09:01 +0200

    chore: Remove unused component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,48 +1,8 @@
-import { Box, Link } from "@mui/material";
-import NextLink from "next/link";
+import { Box } from "@mui/material";
 
-import Flex from "@/components/flex";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Market } from "@/domain/types";
-
-export const HeaderOld = () => {
-  return (
-    <Flex
-      sx={{
-        height: "96px",
-        px: 3,
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      <NextLink href="/">
-        <Flex
-          sx={{
-            backgroundColor: "primary",
-            color: "background",
-            borderRadius: 5,
-            height: "4rem",
-            alignItems: "center",
-            justifyContent: "center",
-            fontSize: 5,
-            textDecoration: "none",
-            px: 5,
-            cursor: "pointer",
-            ":hover": { textDecoration: "none" },
-          }}
-        >
-          Logo
-        </Flex>
-      </NextLink>
-      <Flex>
-        <NextLink href="/create/[chartId]" as="/create/new" passHref>
-          <Link>Get Data</Link>
-        </NextLink>
-      </Flex>
-    </Flex>
-  );
-};
 
 export const AppLayout = ({
   children,
