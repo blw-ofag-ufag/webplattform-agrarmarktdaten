5405c89a37f5fa6b53ab01a1122e36f54fa3baec
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2022-05-31 14:27:45 +0200

    fix: Market subpages

--- src/pages/market/[slug].tsx
+++ src/pages/market/[slug].tsx
@@ -1,42 +1,25 @@
 import { Trans } from "@lingui/macro";
-import { Box, Button, Link, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { GetStaticPaths, GetStaticProps } from "next";
 import React from "react";
 
-import { Banner } from "@/components/banner";
-import { ReportCard } from "@/components/card-data";
-import { TradeLevelsGrid } from "@/components/card-trade";
+import { BlogPostsGrid } from "@/components/blog/BlogPost";
 import { ContentContainer } from "@/components/content-container";
-import Flex from "@/components/flex";
-import { InfografikTeaserLarge } from "@/components/homepage/infografik-teaser";
+import { Hero } from "@/components/hero";
 import { AppLayout } from "@/components/layout";
-import { NewsfeedEntry } from "@/components/newsfeed";
-import { getMarketColor } from "@/domain/colors";
-import { Market, Newsfeed } from "@/domain/types";
-import { IconName } from "@/icons";
+import { BlogPost, Market } from "@/domain/types";
 import { fetchCMS } from "@/lib/cms-api";
 
 export default function MarketPage({
   market,
   allMarkets,
-  allNewsfeeds,
+  allBlogPosts,
 }: {
-  market?: {
-    title: string;
-    description?: string;
-    slug: string;
-    tile: {
-      url: string;
-    };
-    infographic: { title: string; year: string };
-    reports: { title: string; reportid: string }[];
-    links?: { label: string; url: string }[];
-    tradeLevels?: { title: string; icon: IconName }[];
-    children: { title: string; icon: IconName; slug: string }[];
+  market?: Market & {
     _allSlugLocales: { locale: string; value: string }[];
   };
   allMarkets: Market[];
-  allNewsfeeds: Newsfeed[];
+  allBlogPosts: BlogPost[];
 }) {
   const alternates = market
     ? market._allSlugLocales.map((loc) => {
@@ -52,108 +35,17 @@ export default function MarketPage({
     <AppLayout alternates={alternates} allMarkets={allMarkets}>
       {market ? (
         <>
-          <Banner
-            slug={market.slug}
-            title={market.title}
-            intro={market.description}
-          />
-          <Box component="article">
-            <ContentContainer
-              sx={{
-                display: "flex",
-                flexDirection: ["column", "column", "row"],
-                justifyContent: ["flex-start", "flex-start", "space-between"],
-                px: [4, 4, 0],
-                py: 4,
-              }}
-            >
-              <Box sx={{ width: ["100%", "100%", "65%"] }}>
-                <Typography variant="h2">
-                  <Trans id="article.section.infografic">Infografik</Trans>
-                </Typography>
-                {(market.slug === "kartoffeln" ||
-                  market.slug === "potatoes") && <InfografikTeaserLarge />}
-                <Typography variant="h2">
-                  <Trans id="article.section.data">Daten</Trans>
-                </Typography>
-                <Flex
-                  sx={{
-                    flexDirection: ["column", "column", "column"],
-                    justifyContent: [
-                      "flex-start",
-                      "flex-start",
-                      "space-between",
-                    ],
-                  }}
-                >
-                  <ReportCard
-                    type="data"
-                    title={
-                      <Trans id="article.teaser.data.title">
-                        Daten zum Kartoffelmarkt
-                      </Trans>
-                    }
-                    url=""
-                  />
-                  {market.reports.length > 0 && (
-                    <ReportCard
-                      type="report"
-                      title={market.reports[0].title}
-                      url=""
-                    />
-                  )}
-                </Flex>
-                {market.tradeLevels && (
-                  <>
-                    <Typography variant="h2">
-                      <Trans id="article.section.trade">Handelsstufen</Trans>
-                    </Typography>
-                    <TradeLevelsGrid
-                      tradeLevels={market.tradeLevels}
-                      color={getMarketColor(market.slug)}
-                    />
-                  </>
-                )}
-              </Box>
-              <Box sx={{ width: ["100%", "100%", "30%"] }}>
-                <Stack spacing={5}>
-                  <div>
-                    <Typography variant="h2">
-                      <Trans id="article.section.news">Neuigkeiten</Trans>
-                    </Typography>
-                    <div>
-                      {allNewsfeeds.map((news) => (
-                        <NewsfeedEntry
-                          key={news.title}
-                          title={news.title}
-                          publicationDate={news.publicationDate}
-                        />
-                      ))}
-                    </div>
-                    <Button variant="text" sx={{ ml: -2 }}>
-                      <Trans id="button.show.all">Alle Anzeigen</Trans>
-                    </Button>
-                  </div>
-                  <div>
-                    {market.links && (
-                      <>
-                        <Typography variant="h2">
-                          <Trans id="article.section.link">Links</Trans>
-                        </Typography>
-                        {market.links.map((link) => (
-                          <Box key={link.label} sx={{ mb: 3 }}>
-                            <Link href={link.url}>
-                              {">"} {link.label}
-                            </Link>
-                          </Box>
-                        ))}
-                      </>
-                    )}
-                  </div>
-                </Stack>
-              </Box>
-            </ContentContainer>
-          </Box>
+          <Hero title={market.title} lead={market.lead} />
+          <ContentContainer>
+            <Stack flexDirection="column" spacing={6}>
+              <Typography variant="h6">
+                <Trans id="homepage.section.latestBlogPosts">
+                  Latest Blog Posts
+                </Trans>
+              </Typography>
+              <BlogPostsGrid blogPosts={allBlogPosts} />
+            </Stack>
+          </ContentContainer>
         </>
       ) : (
         <div>NOT FOUND</div>
@@ -172,6 +64,11 @@ export const getStaticProps: GetStaticProps = async (context: $FixMe) => {
         tile {
           url
         }
+
+        _allSlugLocales {
+          locale
+          value
+        }
       }
 
       allMarkets(locale: $locale) {
@@ -180,9 +77,20 @@ export const getStaticProps: GetStaticProps = async (context: $FixMe) => {
         slug
       }
 
-      allNewsfeeds(locale: $locale) {
+      allBlogPosts(locale: $locale, first: 3) {
         title
-        publicationDate
+        lead
+        slug
+        image {
+          url
+        }
+        markets {
+          title
+        }
+        themes {
+          title
+        }
+        _firstPublishedAt
       }
     }
   `;
@@ -196,21 +104,21 @@ export const getStaticProps: GetStaticProps = async (context: $FixMe) => {
     props: {
       market: result.market,
       allMarkets: result.allMarkets,
-      allNewsfeeds: result.allNewsfeeds,
+      allBlogPosts: result.allBlogPosts,
     },
   };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const query = `
-  query {
-    allMarkets {
-      _allSlugLocales {
-        locale
-        value
+    query {
+      allMarkets {
+        _allSlugLocales {
+          locale
+          value
+        }
       }
     }
-  }
   `;
 
   const result = await fetchCMS(query);
