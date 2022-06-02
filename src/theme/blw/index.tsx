ae965b4baa7208e636ca54d5d64b685c34fe6303
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2022-06-02 16:01:38 +0200

    chore: Update breakpoints in the BLW theme

--- src/theme/blw/index.tsx
+++ src/theme/blw/index.tsx
@@ -2,6 +2,10 @@ import { createTheme } from "@mui/material/styles";
 
 import federalTheme from "@/theme/federal";
 
-const theme = createTheme(federalTheme, {});
+const theme = createTheme(federalTheme, {
+  breakpoints: {
+    values: { xs: 0, sm: 768, md: 1024, lg: 1280, xl: 1360 },
+  },
+});
 
 export default theme;
