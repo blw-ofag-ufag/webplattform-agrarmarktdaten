d17166b3cfe76c88b5b049b95724d05206cf2c14
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2022-06-15 16:20:08 +0200

    feat: Add BackButton

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,11 @@
 import { Box } from "@mui/material";
+import { useRouter } from "next/router";
 
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Market } from "@/domain/types";
+
+import { BackButton } from "./back-button";
 
 export const AppLayout = ({
   children,
@@ -12,10 +15,17 @@ export const AppLayout = ({
   children: React.ReactNode;
   allMarkets?: Pick<Market, "title" | "slug">[];
   alternates?: { href: string; as: string; locale: string }[];
-}) => (
-  <>
-    <Header alternates={alternates} allMarkets={allMarkets} />
-    <Box sx={{ mt: [0, 0, "92px"], overflow: "auto" }}>{children}</Box>
-    <Footer />
-  </>
-);
+}) => {
+  const router = useRouter();
+
+  return (
+    <>
+      <Header alternates={alternates} allMarkets={allMarkets} />
+      <Box sx={{ mt: [0, 0, "92px"], overflow: "auto", position: "relative" }}>
+        {router.pathname !== "/" ? <BackButton /> : null}
+        {children}
+      </Box>
+      <Footer />
+    </>
+  );
+};
