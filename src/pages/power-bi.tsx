e06b41f096ee6a82ddcf999d52ab42c6011ebd73
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-09-15 12:51:40 +0200

    feat: Fetch Power BI reports from DatoCMS

--- src/pages/power-bi.tsx
+++ src/pages/power-bi.tsx
@@ -1,7 +1,41 @@
 import { PowerBIReport } from "@/components/powerbi-report";
+import * as GQL from "@/graphql";
+import { client } from "@/graphql";
+import { GetStaticProps } from "next";
 
-const Page = () => {
-  return <PowerBIReport datasetId="" reportId="" reportWorkspaceId="" />;
+type Props = GQL.AllPowerBiReportsQuery;
+
+const Page = (props: Props) => {
+  const { allPowerBiReports } = props;
+
+  return allPowerBiReports.map((d) => {
+    const datasetId = d.dataset?.datasetId ?? "";
+    const reportId = d.reportId ?? "";
+    const reportWorkspaceId = d.workspace?.workspaceId ?? "";
+
+    return (
+      <PowerBIReport
+        datasetId={datasetId}
+        reportId={reportId}
+        reportWorkspaceId={reportWorkspaceId}
+      />
+    );
+  });
+};
+
+export const getStaticProps: GetStaticProps<Props> = async (ctx) => {
+  const result = await client
+    .query<GQL.AllPowerBiReportsQuery>(GQL.AllPowerBiReportsDocument, {
+      locale: ctx.locale,
+    })
+    .toPromise();
+
+  if (!result.data) {
+    console.error(result.error?.toString());
+    throw new Error("Failed to fetch API");
+  }
+
+  return { props: result.data };
 };
 
 export default Page;
