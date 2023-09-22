3221af4a20d984326860adbcd5a51a9fb4607f14
João Sobral <joao.tiago.sobral@gmail.com>
2023-09-22 11:14:54 +0200

    chore 🧹: generate icons

--- src/icons/icons-jsx/control/IcControlArrowLeft.js
+++ src/icons/icons-jsx/control/IcControlArrowLeft.js
@@ -1,17 +1,19 @@
 import * as React from "react";
-
-function SvgIcControlArrowLeft(props) {
-  return (
-    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
-      <path
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeWidth={2}
-        d="M10 20l-8-8 8-8m13 8H2"
-      />
-    </svg>
-  );
-}
-
+const SvgIcControlArrowLeft = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeWidth={2}
+      d="m10 20-8-8 8-8m13 8H2"
+    />
+  </svg>
+);
 export default SvgIcControlArrowLeft;
