9f442063281d645215eec4565b70e73f81881518
João Sobral <joao.tiago.sobral@gmail.com>
2023-09-25 14:13:46 +0100

    feat ⚡️: added swiss-federal-ci breakpoints to the theme

--- src/theme/blw/index.tsx
+++ src/theme/blw/index.tsx
@@ -1,10 +1,11 @@
 import { createTheme } from "@mui/material/styles";
+import { breakpoints } from "@interactivethings/swiss-federal-ci";
 
 import federalTheme from "@/theme/federal";
 
 const theme = createTheme(federalTheme, {
   breakpoints: {
-    values: { xs: 0, sm: 768, md: 1024, lg: 1280, xl: 1360 },
+    values: breakpoints,
   },
   components: {
     MuiLink: {
