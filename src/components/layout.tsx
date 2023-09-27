69bd102b647d4860a321235d9287b0f287578901
João Sobral <joao.tiago.sobral@gmail.com>
2023-09-27 11:02:08 +0100

    chore 🧹: missing translation

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -251,7 +251,7 @@ export const AppLayout = ({ children, allMarkets, allFocusArticles, alternates }
               >
                 <FooterSectionTitle title="Feedback" />
                 <Link href="https://www.blw.admin.ch/blw/de/home.html" target="_blank">
-                  <FooterSectionButton label="Contact" />
+                  <FooterSectionButton label="Kontact" />
                 </Link>
               </Box>
             </FooterSection>
