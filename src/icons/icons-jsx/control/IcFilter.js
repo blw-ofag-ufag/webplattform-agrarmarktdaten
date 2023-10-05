ba75645f16e51e7ea43f2be9680d4fd3a2d0d881
Beatriz Malveiro <mbeatrizmalveiro@gmail.com>
2023-10-05 12:56:23 +0100

    feat: add filter icon

---
+++ src/icons/icons-jsx/control/IcFilter.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+const SvgIcFilter = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      fillRule="nonzero"
+      stroke="currentColor"
+      strokeWidth={0.25}
+      d="M14.037 20.563H9.481V11.38L5 8.519V4h13.519v4.519l-4.482 2.86v9.184Zm-3.806-.75h3.056V10.97l4.482-2.86V4.75H5.75v3.358l4.481 2.86v8.845Z"
+    />
+  </svg>
+);
+export default SvgIcFilter;
