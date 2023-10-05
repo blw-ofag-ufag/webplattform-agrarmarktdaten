39d36c15c9a8def3f7051d35074d334d299635b4
Beatriz Malveiro <mbeatrizmalveiro@gmail.com>
2023-10-05 16:34:37 +0100

    feat: style accordion

--- src/theme/blw/index.tsx
+++ src/theme/blw/index.tsx
@@ -2,6 +2,7 @@ import { createTheme } from "@mui/material/styles";
 import { breakpoints } from "@interactivethings/swiss-federal-ci";
 import { deepmerge } from "@mui/utils";
 import federalTheme from "@/theme/federal";
+import { IcControlChevronDown } from "@/icons/icons-jsx/control";
 
 const blwTheme = createTheme({
   breakpoints: {
@@ -16,6 +17,12 @@ const blwTheme = createTheme({
             textDecoration: "none",
           },
         },
+      },
+    },
+
+    MuiAccordionSummary: {
+      defaultProps: {
+        expandIcon: <IcControlChevronDown width={24} height={24} />,
       },
     },
   },
