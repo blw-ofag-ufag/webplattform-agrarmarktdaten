55896fc17e12a0d8df4c8e043ef8a25119fcdadb
Beatriz Malveiro <mbeatrizmalveiro@gmail.com>
2023-10-10 12:44:09 +0200

    feat: time filter component

--- src/components/browser/TimeFilter.tsx
+++ src/components/browser/TimeFilter.tsx
@@ -1,4 +1,4 @@
-import { TimeGranularity, timeAtom } from "@/domain/data";
+import { TimeView } from "@/domain/data";
 import { IcControlCalendar } from "@/icons/icons-jsx/control";
 import { useLocale } from "@/lib/use-locale";
 import { Trans, t } from "@lingui/macro";
@@ -6,10 +6,12 @@ import {
   Button,
   Divider,
   InputLabel,
+  Slider,
   Stack,
   ToggleButton,
   ToggleButtonGroup,
   Typography,
+  sliderClasses,
   toggleButtonClasses,
 } from "@mui/material";
 import {
@@ -22,7 +24,7 @@ import dayjs, { Dayjs } from "dayjs";
 import "dayjs/locale/de";
 import "dayjs/locale/fr";
 import "dayjs/locale/it";
-import { useAtom } from "jotai";
+import { useEffect, useMemo, useState } from "react";
 import { withStyles } from "../style-utils";
 
 const FilterToggleButton = withStyles(ToggleButton, (theme) => ({
@@ -44,12 +46,88 @@ const FilterToggleButton = withStyles(ToggleButton, (theme) => ({
   },
 }));
 
-export default function TimeFilter() {
-  const [time, setTime] = useAtom(timeAtom);
+const TimeSlider = withStyles(Slider, (theme) => ({
+  root: {
+    [`& .${sliderClasses.rail}`]: {
+      backgroundColor: theme.palette.grey[600],
+    },
+    [`& .${sliderClasses.track}`]: {
+      height: "2px",
+      borderRadius: "4px",
+    },
+    [`& .${sliderClasses.thumb}`]: {
+      height: "16px",
+      width: "16px",
+      borderRadius: "50%",
+      backgroundColor: theme.palette.grey[800],
+      [`&:hover, &:focus, &.${sliderClasses.active}`]: {
+        /* hardcoded, but probably will be changed with the new theme anyway */
+        boxShadow: "0px 0px 0px 4px rgba(68, 68, 68, 0.16);",
+      },
+    },
+    [`& .${sliderClasses.mark}`]: {
+      display: "none",
+    },
+    [`& .${sliderClasses.markLabel}`]: {
+      top: "24px",
+    },
+  },
+}));
+
+export default function TimeFilter({
+  min,
+  max,
+  value,
+  view,
+  onChangeRange = () => {},
+  onChangeView = () => {},
+}: {
+  min: number;
+  max: number;
+  value: [number, number];
+  view: TimeView;
+  onChangeRange?: (newValues: [number, number]) => void;
+  onChangeView?: (newGranularity: TimeView) => void;
+}) {
+  const minDate = useMemo(() => dayjs.unix(min), [min]);
+  const maxDate = useMemo(() => dayjs.unix(max), [max]);
+  const [sliderRange, setSliderRange] = useState(value);
+
+  useEffect(() => {
+    setSliderRange(value);
+  }, [value]);
+
   const locale = useLocale();
 
+  const timeFormat = useMemo(
+    () => (view === "year" ? "YYYY" : "MM.YYYY"),
+    [view]
+  );
+
+  const sliderValues = useMemo(() => {
+    const getMark = (date: Dayjs, showLabel: boolean = false) => ({
+      value: date.unix(),
+      ...(showLabel && { label: date.format(timeFormat) }),
+    });
+
+    const values = [getMark(minDate, true)];
+
+    let current = minDate.clone();
+    while (current.isBefore(maxDate) && !current.isSame(maxDate)) {
+      current = current.add(1, view);
+      if (!current.isSame(maxDate)) {
+        values.push(getMark(current));
+      }
+    }
+
+    values.push(getMark(maxDate, true));
+
+    return values;
+  }, [minDate, maxDate, view, timeFormat]);
+  console.log(sliderValues);
+
   return (
-    <Stack gap={1}>
+    <Stack gap={3}>
       <Button
         variant="text"
         sx={{
@@ -62,9 +140,9 @@ export default function TimeFilter() {
       <ToggleButtonGroup
         size="small"
         fullWidth
-        value={time}
+        value={view}
         exclusive
-        onChange={(_, value) => setTime(value)}
+        onChange={(_, value) => onChangeView(value)}
       >
         <FilterToggleButton value="year">
           <Trans id="data.filters.year">Year</Trans>
@@ -76,9 +154,15 @@ export default function TimeFilter() {
       <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={locale}>
         <Stack direction="row" spacing={3} alignItems="end">
           <DatePickerField
-            time={time}
+            min={minDate}
+            max={maxDate}
             label={t({ id: "data.filters.from", message: "From" })}
-            views={time === "year" ? ["year"] : ["year", "month"]}
+            views={view === "year" ? ["year"] : ["year", "month"]}
+            format={timeFormat}
+            value={dayjs.unix(value[0])}
+            onChange={(date) => {
+              if (date) onChangeRange([date.unix(), value[1]]);
+            }}
           />
           <Divider
             orientation="horizontal"
@@ -92,22 +176,53 @@ export default function TimeFilter() {
             }}
           />
           <DatePickerField
-            time={time}
+            min={minDate}
+            max={maxDate}
             label={t({ id: "data.filters.from", message: "To" })}
-            views={time === "year" ? ["year"] : ["year", "month"]}
+            views={view === "year" ? ["year"] : ["year", "month"]}
+            format={timeFormat}
+            value={dayjs.unix(value[1])}
+            onChange={(date) => {
+              if (date) onChangeRange([value[0], date.unix()]);
+            }}
           />
         </Stack>
+        <TimeSlider
+          value={sliderRange}
+          min={min}
+          max={max}
+          step={null}
+          size="small"
+          marks={sliderValues}
+          onChange={(_, value) => setSliderRange(value as [number, number])}
+          onChangeCommitted={(_, value) =>
+            onChangeRange(value as [number, number])
+          }
+          sx={{
+            [`& .${sliderClasses.markLabel}[data-index="0"]`]: {
+              transform: "translateX(0%)",
+            },
+            [`& .${sliderClasses.markLabel}[data-index="${
+              sliderValues.length - 1
+            }"]`]: {
+              transform: "translateX(-100%)",
+            },
+          }}
+        />
       </LocalizationProvider>
     </Stack>
   );
 }
 
 const DatePickerField = ({
-  time,
+  min,
+  max,
   label,
   ...props
 }: {
-  time: TimeGranularity;
+  min: Dayjs;
+  max: Dayjs;
+  format: string;
   label: string;
 } & DatePickerProps<Dayjs>) => {
   return (
@@ -116,10 +231,8 @@ const DatePickerField = ({
         <Typography variant="caption">{label}</Typography>
       </InputLabel>
       <DatePicker
-        minDate={dayjs("2020-01")}
-        maxDate={dayjs("2023-01")}
-        format={time === "year" ? "YYYY" : "MM.YYYY"}
-        views={time === "year" ? ["year"] : ["year", "month"]}
+        minDate={min}
+        maxDate={max}
         slots={{
           openPickerIcon: IcControlCalendar,
         }}
