5ddc71237dc07ae0ca8a495887fba3c0c5fe110c
Patrick Browne <pt.browne@gmail.com>
2023-10-24 13:35:14 +0200

    refactor: Extract colors from market chip

--- src/components/MarketChip/MarketChip.tsx
+++ src/components/MarketChip/MarketChip.tsx
@@ -1,6 +1,5 @@
-import * as React from "react";
-import { Chip } from "@mui/material";
 import { makeStyles } from "@/components/style-utils";
+import { Chip } from "@mui/material";
 
 interface Props {
   slug?: string | null;
@@ -14,120 +13,107 @@ const useStyles = makeStyles()({
     lineHeight: "18px",
     fontSize: "14px",
   },
+  milk: {
+    backgroundColor: "#6C84B5",
+    color: "#1F2937",
+  },
+  "milk-and-meat-substitutes": {
+    backgroundColor: "#B0BDD7",
+    color: "#30415E",
+  },
+  "fruits-and-vegetables": {
+    backgroundColor: "#C5E0B2",
+    color: "#495B46",
+  },
+  meat: {
+    backgroundColor: "#FBCDC8",
+    color: "#A8322D",
+  },
+
+  "bread-and-cereals": {
+    backgroundColor: "#FDCC95",
+    color: "#7B441F",
+  },
+  eggs: {
+    backgroundColor: "#F7EBB6",
+    color: "#695501",
+  },
+  feed: {
+    backgroundColor: "#825B40",
+    color: "#F9FAFB",
+  },
+  potatoes: {
+    backgroundColor: "#CFBCAA",
+    color: "#5B4632",
+  },
+
+  "oil-seeds": {
+    backgroundColor: "#AA8F1F",
+    color: "#111827",
+  },
+  default: {
+    backgroundColor: "#DFE4E9",
+    color: "#1F2937",
+  },
 });
 
-const MarketChip = (props: Props) => {
-  const { slug, label } = props;
-  const { classes } = useStyles();
+const getTypeFromSlug = (slug: string | undefined | null) => {
   switch (slug) {
     case "milch-und-milchprodukte":
     case "lait-et-produits-laitiers":
     case "latte-e-latticini":
     case "milk":
-      return (
-        <Chip
-          className={classes.chip}
-          sx={{ backgroundColor: "#6C84B5", color: "#1F2937" }}
-          label={label}
-        />
-      );
+      return "milk";
     case "milch-und-fleischersatzprodukte":
     case "lait-et-substituts-de-viande":
     case "sostituti-del-latte-e-della-carne":
     case "milk-and-meat-substitutes":
-      return (
-        <Chip
-          className={classes.chip}
-          sx={{ backgroundColor: "#B0BDD7", color: "#30415E" }}
-          label={label}
-        />
-      );
+      return "milk-and-meat-substitutes";
     case "fruchte-gemuese-speisepilze":
     case "fruits-et-legumes":
     case "frutta-e-verdura":
     case "fruits-and-vegetables":
-      return (
-        <Chip
-          className={classes.chip}
-          sx={{ backgroundColor: "#C5E0B2", color: "#495B46" }}
-          label={label}
-        />
-      );
+      return "fruits-and-vegetables";
     case "fleisch":
     case "viande":
     case "carne":
     case "meat":
-      return (
-        <Chip
-          className={classes.chip}
-          sx={{ backgroundColor: "#FBCDC8", color: "#A8322D" }}
-          label={label}
-        />
-      );
+      return "meat";
     case "getreide":
     case "pain-et-ceraales":
     case "pane-e-cereali":
     case "bread-and-cereals":
-      return (
-        <Chip
-          className={classes.chip}
-          sx={{ backgroundColor: "#FDCC95", color: "#7B441F" }}
-          label={label}
-        />
-      );
+      return "bread-and-cereals";
     case "eier":
     case "oeufs":
     case "uova":
     case "eggs":
-      return (
-        <Chip
-          className={classes.chip}
-          sx={{ backgroundColor: "#F7EBB6", color: "#695501" }}
-          label={label}
-        />
-      );
+      return "eggs";
     case "futtermittel":
     case "alimentation":
     case "alimentazione":
     case "feed":
-      return (
-        <Chip
-          className={classes.chip}
-          sx={{ backgroundColor: "#825B40", color: "#F9FAFB" }}
-          label={label}
-        />
-      );
+      return "feed";
     case "kartoffeln":
     case "pommes-de-terre":
     case "patate":
     case "potatoes":
-      return (
-        <Chip
-          className={classes.chip}
-          sx={{ backgroundColor: "#CFBCAA", color: "#5B4632" }}
-          label={label}
-        />
-      );
+      return "potatoes";
     case "olsaaten":
     case "graines-oleagineuses":
     case "semi-oleosi":
     case "oil-seeds":
-      return (
-        <Chip
-          className={classes.chip}
-          sx={{ backgroundColor: "#AA8F1F", color: "#111827" }}
-          label={label}
-        />
-      );
+      return "oil-seeds";
     default:
-      return (
-        <Chip
-          className={classes.chip}
-          sx={{ backgroundColor: "#DFE4E9", color: "#1F2937" }}
-          label={label}
-        />
-      );
+      return "default";
   }
+};
+
+const MarketChip = (props: Props) => {
+  const { slug, label } = props;
+  const { classes, cx } = useStyles();
+  const type = getTypeFromSlug(slug);
+  return <Chip label={label} className={cx(classes.chip, classes[type])} />;
 };
 
 export default MarketChip;
