751ef6d1a1e3c5db37988c5323d1c77ce04258d1
Patrick Browne <pt.browne@gmail.com>
2023-10-25 23:18:04 +0200

    wip

--- src/theme/federal/index.tsx
+++ src/theme/federal/index.tsx
@@ -39,14 +39,8 @@ declare module "@mui/material" {
   interface PaletteOptions {
     muted?: PaletteColorOptions;
     alert?: PaletteColorOptions;
-    organization?: PaletteColorOptions;
-    category?: PaletteColorOptions;
     brand?: PaletteColorOptions;
     hint?: PaletteColorOptions;
-  }
-
-  interface TypographyPropsVariantOverrides {
-    tag: true;
   }
 
   interface ButtonPropsVariantOverrides {
