6fff1aafca7232b14257d0a1e07a7f5dabe5a3ad
Patrick Browne <pt.browne@gmail.com>
2023-10-25 22:28:32 +0200

    fix: Add weight 400 for fonts since body1 needs it

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -19,7 +19,7 @@ const config: StorybookConfig = {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@500;700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;700&display=swap"
         rel="stylesheet"
       />
 
