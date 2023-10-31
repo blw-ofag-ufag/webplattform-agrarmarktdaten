da0bd6810ad1823f015c21207a088457829a398c
João Sobral <joao.tiago.sobral@gmail.com>
2023-10-31 13:44:54 +0000

    feat ⚡️: setup search client

---
+++ src/lib/dato.ts
@@ -0,0 +1,19 @@
+import { buildClient } from "@datocms/cma-client-browser";
+import { DATOCMS_API_TOKEN } from "@/domain/env";
+
+export const client = buildClient({ apiToken: DATOCMS_API_TOKEN ?? "" });
+
+export interface DatoSearch {
+  type: "search_result";
+  id: string;
+  attributes: {
+    title: string;
+    body_excerpt: string;
+    url: string;
+    score: number;
+    highlight: {
+      title: string[];
+      body: string[];
+    };
+  };
+}
