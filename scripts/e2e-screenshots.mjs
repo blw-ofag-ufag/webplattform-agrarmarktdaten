f35d57dac62437327a02f32355c46973510f8f79
Patrick Browne <pt.browne@gmail.com>
2023-11-06 13:52:17 +0100

    feat: Add screenshots

---
+++ scripts/e2e-screenshots.mjs
@@ -0,0 +1,40 @@
+import { chromium, devices } from "playwright";
+import { breakpoints } from "@interactivethings/swiss-federal-ci";
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false,
+  });
+
+  // List of pages to visit
+  const urls = ["/analysis", "/methods", "/data", "/"];
+
+  for (const url of urls) {
+    const pageName = url === "/" ? "home" : url.replace("/", ""); // Name for the screenshot file
+
+    const pageContext = await browser.newContext({
+      ...devices["Desktop 1920x1080"], // Set an initial viewport size
+    });
+
+    const page = await pageContext.newPage();
+    await page.goto(`http://localhost:3000/${url}`);
+
+    for (const [name, width] of Object.entries(breakpoints)) {
+      // Set viewport size for the current breakpoint
+      await page.setViewportSize({
+        width: width + (name === "xl" || name === "xxl" || name === "xxxl" ? 2 * 16 : 0),
+        height: 1080,
+      });
+
+      // Take a screenshot and save it with page name and breakpoint name
+      const screenshotPath = `screenshots/${pageName}-${name}.png`; // You can adjust the path as needed
+      await page.screenshot({ path: screenshotPath });
+
+      console.log(`Screenshot taken: ${screenshotPath}`);
+    }
+
+    await pageContext.close();
+  }
+
+  await browser.close();
+})();
