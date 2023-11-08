dd5b4c3c6c468b1327121ac401d56e7aefe6e090
Patrick Browne <pt.browne@gmail.com>
2023-11-08 23:16:43 +0100

    feat: Customize just a bit the storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,7 +10,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm"
+    "@storybook/addon-mdx-gfm",
   ],
   previewHead: (head) => {
     // Need to be in sync with what's in _document
