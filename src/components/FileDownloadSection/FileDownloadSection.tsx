aa903df836a20b66993c19cb8f7dfb8936f88a8e
Patrick Browne <pt.browne@gmail.com>
2023-11-08 10:44:06 +0100

    feat: Refactor to use styles & align with design FileDownloadSection

--- src/components/FileDownloadSection/FileDownloadSection.tsx
+++ src/components/FileDownloadSection/FileDownloadSection.tsx
@@ -1,45 +1,77 @@
 import * as React from "react";
 import { Typography, Box } from "@mui/material";
 import * as GQL from "@/graphql";
-import { s, c } from "@interactivethings/swiss-federal-ci";
 import { Temporal } from "proposal-temporal";
 import Link from "next/link";
 import { Download } from "@/icons/icons-jsx/control";
 import { Intersperse } from "@/components/Intersperse";
 import { makeStyles } from "@/components/style-utils";
 
-const useStyles = makeStyles()(({ palette: c }) => ({
-  root: {
-    position: "relative",
-    border: `${c.cobalt[100]} 4px solid`,
-    borderRadius: "12px",
-    backgroundColor: `${c.cobalt[50]}`,
-    paddingX: s(20),
-    paddingBottom: s(12),
-    paddingTop: s(15),
-  },
+const useStyles = makeStyles<void, "fileTitle" | "file">()(
+  ({ palette: c, spacing: s }, _params, classes) => ({
+    root: {
+      position: "relative",
+      border: `${c.cobalt[100]} 4px solid`,
+      borderRadius: "12px",
+      backgroundColor: `${c.cobalt[50]}`,
+      paddingLeft: "4.375rem",
+      paddingRight: "4.375rem",
+      paddingBottom: "3rem",
+      paddingTop: "3rem",
+    },
 
-  legend: {
-    position: "absolute",
-    top: -15,
-    left: 80,
-    backgroundColor: c.cobalt[100],
-    width: "fit-content",
-    borderRadius: 100,
-    padding: s(3, 1),
-  },
+    legend: {
+      position: "absolute",
+      top: -15,
+      left: 80,
+      backgroundColor: c.cobalt[100],
+      width: "fit-content",
+      borderRadius: "9999px",
+      padding: "0.375rem 1.125rem",
+    },
 
-  separator: {
-    height: "1px",
-    width: "100%",
-    backgroundColor: c.cobalt[200],
-  },
+    separator: {
+      height: "1px",
+      width: "100%",
+      backgroundColor: c.cobalt[200],
+    },
 
-  title: {
-    fontWeight: 700,
-    color: c.cobalt[800],
-  },
-}));
+    title: {
+      fontWeight: 700,
+      color: c.cobalt[800],
+    },
+
+    file: {
+      cursor: "pointer",
+      padding: s(4, 0),
+      "&:hover": {
+        backgroundColor: c.cobalt[50],
+        [`& .${classes.fileTitle}`]: { textDecoration: "underline" },
+      },
+      color: c.monochrome[800],
+      display: "grid",
+      gridTemplateColumns: "min-content 1fr",
+      gridTemplateRows: "min-content min-content",
+      gridTemplateAreas: '"icon title" "empty content"',
+      columnGap: "1rem",
+    },
+    fileTitle: {
+      color: c.monochrome[800],
+      gridArea: "title",
+      display: "flex",
+      alignItems: "center",
+    },
+    fileIcon: { gridArea: "icon", display: "flex", color: c.cobalt[800] },
+    fileContent: {
+      marginTop: s(3),
+      color: c.monochrome[500],
+      gridArea: "content",
+    },
+    fileDescription: {
+      marginBottom: s(2),
+    },
+  })
+);
 
 interface Props {
   data: Partial<GQL.FileDownloadSectionRecord>;
@@ -70,35 +102,23 @@ const FileDownloadSection = (props: Props) => {
           return (
             file?.url && (
               <Link href={file.url} key={id} legacyBehavior>
-                <Box
-                  sx={{
-                    cursor: "pointer",
-                    py: s(4),
-                    ":hover": {
-                      backgroundColor: c.cobalt[50],
-                      ".title": { textDecoration: "underline" },
-                    },
-                  }}
-                >
-                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                <Box className={classes.file}>
+                  <div className={classes.fileIcon}>
                     <Download width={24} height={24} />
-                    <Typography
-                      className="title"
-                      variant="h3"
-                      sx={{ ml: s(4), color: c.cobalt[800] }}
-                    >
-                      {title}
-                    </Typography>
-                  </Box>
-                  <Box sx={{ ml: s(10.5), mt: s(3), color: c.cobalt[500] }}>
-                    <Typography variant="body1" sx={{ mb: s(2) }}>
+                  </div>
+                  <Typography className={classes.fileTitle} variant="h4">
+                    {title}
+                  </Typography>
+
+                  <div className={classes.fileContent}>
+                    <Typography variant="body1" className={classes.fileDescription} display="block">
                       {description}
                     </Typography>
-                    <Typography variant="body1" sx={{ fontWeight: 700 }}>
+                    <Typography variant="h5" component="p">
                       {`${file?.format.toUpperCase()}`} &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;{" "}
                       {formattedDate}
                     </Typography>
-                  </Box>
+                  </div>
                 </Box>
               </Link>
             )
