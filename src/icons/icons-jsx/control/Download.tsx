cba7d72d1277971a08c752fcbf340b9e259e0808
Patrick Browne <pt.browne@gmail.com>
2023-11-09 12:31:08 +0100

    refactor: Use typescript for icons

---
+++ src/icons/icons-jsx/control/Download.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgDownload = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" {...props}>
+    <g fill="#1F2937" stroke="#1F2937" strokeWidth={0.25}>
+      <path d="m19.088 13.243-.375-.649-6.294 3.634V4h-.75v12.228l-6.294-3.634-.375.649 7.044 4.067zM5.675 19.462h12.768v.75H5.675z" />
+    </g>
+  </svg>
+);
+export default SvgDownload;
---
+++ src/icons/icons-jsx/markets/IcMarketFruitVeggie.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgIcMarketFruitVeggie = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64" {...props}>
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeWidth={2}
+      d="M29.401 58.38c-1.504-.185-2.845-.448-3.962-.448-4.267 0-11.807 3.835-19.284-7.477-7.793-11.79-5.875-25.13-.097-29.526 6.296-4.793 13.38-1.342 19.38-1.342 6.001 0 13.083-3.45 19.382 1.342 2.997 2.28 4.955 6.969 5.058 12.6M38 3v3a8 8 0 0 1-8 8v-3a8 8 0 0 1 8-8ZM26 19c0-5.085-4.16-11.41-10-13m14.3 55.879a3.4 3.4 0 0 1-.287-4.624l18.02-22.13c2.636-3.238 7.611-3.525 10.628-.613 3.017 2.911 2.72 7.712-.635 10.255L35.092 62.156c-1.44 1.09-3.498.973-4.792-.277Zm3.286-7.664 2.173 2.172m15.21-14.115-4.346-4.343m-3.26 11.943 3.26 3.258m10.864-19.544c1.245-2.65 3.87-3.258 6.518-3.258"
+    />
+  </svg>
+);
+export default SvgIcMarketFruitVeggie;
