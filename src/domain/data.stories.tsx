1929673132767a6d928fda09aa4c114a27977ab2
Beatriz Malveiro <mbeatrizmalveiro@gmail.com>
2023-11-13 11:22:09 +0100

    feat: add storybook stories for data

--- src/domain/data.stories.tsx
+++ src/domain/data.stories.tsx
@@ -1,26 +1,167 @@
-import { Typography } from "@mui/material";
+import { amdpMeasure } from "@/lib/namespace";
+import { Locale, locales } from "@/locales/locales";
+import { CubeSpec, fetchCubeDimensions, fetchObservations } from "@/pages/api/data";
+import {
+  CircularProgress,
+  FormControlLabel,
+  MenuItem,
+  Select,
+  Stack,
+  Switch,
+  Typography,
+} from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { useQuery } from "@tanstack/react-query";
 import { useAtomValue } from "jotai";
-import { cubesAtom } from "./cubes";
-import { CubeSpec } from "@/pages/api/data";
+import { useMemo, useState } from "react";
+import ReactJson from "react-json-view";
+import { cubesAtom, defaultCube } from "./cubes";
+import { baseDimensionsAtom } from "./dimensions";
+import { valueFormatter } from "./observations";
 
 export const AvailableCubes = () => {
   const cubes = useAtomValue(cubesAtom);
   const columns: Array<GridColDef & { field: keyof CubeSpec }> = [
-    { field: "cube", headerName: "Cube", editable: false },
-    { field: "valueChain", headerName: "Cube", editable: false },
-    { field: "market", headerName: "Cube", editable: false },
-    { field: "timeView", headerName: "Time View", editable: false },
+    { field: "cube", headerName: "Cube", editable: false, width: 300 },
+    { field: "valueChain", headerName: "Value Chain", editable: false, width: 150 },
+    { field: "market", headerName: "Market", editable: false, width: 150 },
+    { field: "measure", headerName: "Measure", editable: false, width: 150 },
+    { field: "timeView", headerName: "Time View", editable: false, width: 150 },
   ];
 
   return (
-    <>
-      <Typography variant="h1">Available Cubes</Typography>
-      <DataGrid rows={cubes} columns={columns} autoHeight={true} />
-    </>
+    <Stack gap={2}>
+      <Typography variant="h2">Available Cubes</Typography>
+      <DataGrid rows={cubes} getRowId={(row) => row.cube} columns={columns} autoHeight={true} />
+    </Stack>
+  );
+};
+
+export const BaseDimensions = () => {
+  const baseDimensions = useAtomValue(baseDimensionsAtom);
+
+  return (
+    <Stack gap={2}>
+      <Typography variant="h2">Base Dimensions</Typography>
+      <ReactJson src={baseDimensions} />
+    </Stack>
+  );
+};
+
+export const CubeDimensions = () => {
+  const [cube, setCube] = useState<string>(defaultCube);
+  const [locale, setLocale] = useState<Locale>("de");
+  const cubes = useAtomValue(cubesAtom);
+
+  const dimensions = useQuery({
+    queryKey: ["dimensions", cube, locale],
+    queryFn: () => fetchCubeDimensions(locale, cube),
+  });
+
+  return (
+    <Stack gap={2}>
+      <Typography variant="h2">Cube Dimensions</Typography>
+      <Select label="Cube" value={cube} onChange={(e) => setCube(e.target.value)}>
+        {cubes.map((c) => (
+          <MenuItem value={c.cube}>{c.cube}</MenuItem>
+        ))}
+      </Select>
+      <Select label="Language" value={locale} onChange={(e) => setLocale(e.target.value as Locale)}>
+        {locales.map((l) => (
+          <MenuItem value={l}>{l.toUpperCase()}</MenuItem>
+        ))}
+      </Select>
+      {dimensions.isLoading && <CircularProgress />}
+      {dimensions.isSuccess && <ReactJson src={dimensions.data} />}
+    </Stack>
+  );
+};
+
+export const Observations = () => {
+  const [cube, setCube] = useState<string>(defaultCube);
+  const [showParsed, setShowParsed] = useState(false);
+  const cubes = useAtomValue(cubesAtom);
+  const cubeDef = cubes.find((c) => c.cube === cube);
+
+  console.log(cube, cubeDef);
+
+  const observations = useQuery({
+    queryKey: ["observations", cube],
+    queryFn: () =>
+      fetchObservations({
+        cubeIri: cube,
+        measure: {
+          iri: amdpMeasure(cubeDef?.measure).value,
+          key: cubeDef?.measure ?? "measure",
+        },
+      }),
+  });
+
+  const dimensions = useQuery({
+    queryKey: ["dimensions", cube],
+    queryFn: () => fetchCubeDimensions("de", cube),
+  });
+
+  const columns: GridColDef[] = useMemo(() => {
+    if (!observations.data || observations.data.length === 0) {
+      return [];
+    }
+
+    return Object.entries(observations.data[0]).map(([key, value]) => {
+      return {
+        field: key,
+        headerName: key,
+        width: 200,
+        valueFormatter: (params) => {
+          if (dimensions.data && showParsed) {
+            return valueFormatter({
+              value: params.value,
+              dimension: key,
+              cubeDimensions: dimensions.data,
+            });
+          }
+          return params.value;
+        },
+      };
+    });
+  }, [observations.data, showParsed]);
+
+  console.log(observations);
+  return (
+    <Stack gap={2}>
+      <Typography variant="h2">Cube Observations</Typography>
+      <Select label="Cube" value={cube} onChange={(e) => setCube(e.target.value)}>
+        {cubes.map((c) => (
+          <MenuItem value={c.cube}>{c.cube}</MenuItem>
+        ))}
+      </Select>
+      <FormControlLabel
+        sx={{
+          ml: 1,
+        }}
+        control={
+          <Switch
+            defaultChecked
+            checked={showParsed}
+            onChange={(e) => setShowParsed(e.target.checked)}
+          />
+        }
+        label="Show parsed"
+      />
+      {observations.isLoading && <CircularProgress />}
+      {observations.isSuccess && observations.data && (
+        <DataGrid
+          rows={observations.data}
+          getRowId={(row) => row.observation}
+          columns={columns}
+          autoHeight
+        />
+      )}
+    </Stack>
   );
 };
 
 export default {
-  component: AvailableCubes,
+  component: BaseDimensions,
+  title: "Domain / Data",
 };
