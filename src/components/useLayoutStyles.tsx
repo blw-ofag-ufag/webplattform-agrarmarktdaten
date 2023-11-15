7543cb45e0f1c9a113f3a820b3a5fa0dc4140f8c
Patrick Browne <pt.browne@gmail.com>
2023-11-15 10:45:53 +0100

    fix: Remove margin left on content when aside is displayed none + adjusts number of columns

--- src/components/useLayoutStyles.tsx
+++ src/components/useLayoutStyles.tsx
@@ -4,35 +4,43 @@ import { useStickyBox } from "react-sticky-box";
 
 const ASIDE_NB_COLUMNS = 2;
 
+// Due to how gridColumn is implemented, we need to override the margin-left
+// to be 0 in cases when the first grid element (the aside) is displayed none
+const noMarginLeft = {
+  "&&": {
+    marginLeft: 0,
+  },
+};
+
 export const useLayoutStyles = makeStyles()((theme) => ({
   aside: {
-    [theme.breakpoints.up("xl")]: {
+    [theme.breakpoints.up("xxl")]: {
       ...gridColumn(ASIDE_NB_COLUMNS),
       // Prevent table of contents to stick to footer when at bottom
       // of the page
       paddingBottom: 124,
     },
-    [theme.breakpoints.down("xl")]: { display: "none" },
+    [theme.breakpoints.down("xxl")]: { display: "none" },
     height: "fit-content",
   },
   content: {
-    [theme.breakpoints.up("xl")]: gridColumn(10),
-    [theme.breakpoints.down("xl")]: gridColumn(6),
-    [theme.breakpoints.down("lg")]: gridColumn(4),
+    [theme.breakpoints.up("xxl")]: gridColumn(8),
+    [theme.breakpoints.down("xxl")]: { ...gridColumn(12), ...noMarginLeft },
+    [theme.breakpoints.down("xl")]: { ...gridColumn(6), ...noMarginLeft },
   },
 }));
 
-export const useHeroStyles = makeStyles<{ shiftedLeft: boolean }>()((theme) => ({
+export const useHeroStyles = makeStyles<{ shiftedLeft: boolean }>()((theme, { shiftedLeft }) => ({
   shifter: {
-    [theme.breakpoints.up("xl")]: gridColumn(ASIDE_NB_COLUMNS),
-    [theme.breakpoints.down("xl")]: {
+    [theme.breakpoints.up("xxl")]: gridColumn(ASIDE_NB_COLUMNS),
+    [theme.breakpoints.down("xxl")]: {
       display: "none",
     },
   },
   heroContent: {
-    [theme.breakpoints.up("xl")]: gridColumn(10),
-    [theme.breakpoints.down("xl")]: gridColumn(6),
-    [theme.breakpoints.down("lg")]: gridColumn(4),
+    [theme.breakpoints.up("xxl")]: gridColumn(10 - (shiftedLeft ? 2 : 0)),
+    [theme.breakpoints.down("xxl")]: { ...gridColumn(12), ...noMarginLeft },
+    [theme.breakpoints.down("xl")]: { ...gridColumn(6), ...noMarginLeft },
   },
 }));
 
