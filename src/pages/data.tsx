be082228a64cfc4964f4125d20b794d26808216a
Patrick Browne <pt.browne@gmail.com>
2023-11-16 10:27:49 +0100

    feat: Add a debugger for data atoms

--- src/pages/data.tsx
+++ src/pages/data.tsx
@@ -26,10 +26,13 @@ import React, { PropsWithChildren, Suspense, useEffect, useMemo, useState } from
 import SidePanel from "@/components/browser/SidePanel";
 import { cubeDimensionsAtom } from "@/domain/dimensions";
 import { observationsAtom, observationsQueryAtom, valueFormatter } from "@/domain/observations";
-import { IcControlArrowRight, IcControlDownload } from "@/icons/icons-jsx/control";
+import { IcControlArrowRight, IcControlClose, IcControlDownload } from "@/icons/icons-jsx/control";
 import { Trans, plural, t } from "@lingui/macro";
 import { Circle } from "@mui/icons-material";
 import { Measure, Observation, Property } from "./api/data";
+import { ObjectInspector } from "react-inspector";
+import { useFlag } from "@/utils/flags";
+import { makeStyles } from "@/components/style-utils";
 
 const blackAndWhiteTheme = createTheme(blwTheme, {
   palette: {
@@ -88,6 +91,58 @@ export default function DataPage(props: GQL.DataPageQuery) {
   );
 }
 
+const useDebugStyles = makeStyles()((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+    position: "fixed",
+    right: 0,
+    zIndex: 1,
+    background: "paper",
+    maxWidth: 400,
+  },
+}));
+
+const DebugDataBrowser = () => {
+  const observations = useAtomValue(observationsAtom);
+  const observationsQuery = useAtomValue(observationsQueryAtom);
+  const cubeDimensions = useAtomValue(cubeDimensionsAtom);
+  const [expanded, setExpanded] = useState(false);
+  const { classes } = useDebugStyles();
+  return (
+    <Paper className={classes.root}>
+      {expanded ? (
+        <>
+          <IconButton
+            size="small"
+            onClick={() => setExpanded(false)}
+            sx={{ position: "absolute", top: "0.25rem", right: "0.25rem" }}
+          >
+            <IcControlClose fontSize="small" />
+          </IconButton>
+          <Stack spacing={2} sx={{ fontSize: "small" }}>
+            <div>
+              <Typography variant="h5">Observations</Typography>
+              <ObjectInspector data={observations.observations} />
+            </div>
+            <div>
+              <Typography variant="h5">Observations query status</Typography>
+              <ObjectInspector data={observationsQueryStatus} />
+            </div>
+            <div>
+              <Typography variant="h5">Cube dimensions</Typography>
+              <ObjectInspector data={cubeDimensions} />
+            </div>
+          </Stack>
+        </>
+      ) : (
+        <Button size="small" onClick={() => setExpanded(true)}>
+          debug
+        </Button>
+      )}
+    </Paper>
+  );
+};
+
 const DataBrowser = () => {
   const [showMetadataPanel, setShowMetadataPanel] = useState(false);
   const contentRef = React.useRef<HTMLDivElement>(null);
@@ -96,8 +151,11 @@ const DataBrowser = () => {
   const resultCount = observations.observations.length;
   const cubeDimensions = useAtomValue(cubeDimensionsAtom);
 
+  const debug = useFlag("debug");
+
   return (
     <Stack direction="row" width="100%" ref={contentRef}>
+      {debug ? <DebugDataBrowser /> : null}
       <Box width="388px" flexGrow={0} flexShrink={0}>
         <SidePanel />
       </Box>
