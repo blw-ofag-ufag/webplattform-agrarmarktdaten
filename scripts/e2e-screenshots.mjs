10284624b5e7735b7fe87681f1b2178e67443d2a
Patrick Browne <pt.browne@gmail.com>
2023-11-21 15:07:04 +0100

    feat: Screenshot milk page

--- scripts/e2e-screenshots.mjs
+++ scripts/e2e-screenshots.mjs
@@ -12,7 +12,7 @@ const main = async () => {
     { route: "/analysis", fullPage: false },
     { route: "/methods", fullPage: false },
     { route: "/legal", fullPage: false },
-    { route: "/en/blog/demo-de", fullPage: true },
+    { route: "/de/market/milch-und-milchprodukte", fullPage: true },
     { route: "/", fullPage: true },
   ];
 
