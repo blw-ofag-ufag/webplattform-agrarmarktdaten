8e35437f04ed4b555e1aff216fd8d2540c67c45c
João Sobral <joao.tiago.sobral@gmail.com>
2023-11-21 12:26:49 +0000

    chore 🧹: add xmlns:xhtml to sitemap.xml

--- src/pages/sitemap.xml/index.tsx
+++ src/pages/sitemap.xml/index.tsx
@@ -15,7 +15,7 @@ interface Paths {
 
 function generateSiteMap(blogposts: Paths[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
       ${blogposts
         .map(({ locale, params: { slug } }) => {
           return `
