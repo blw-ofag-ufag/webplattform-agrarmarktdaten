b6c17ae56050fb54824a48a7bb857d0802451c76
Patrick Browne <pt.browne@gmail.com>
2023-11-23 10:04:48 +0100

    fix: Adjust font size for power bi navigation report

--- src/components/powerbi-report.tsx
+++ src/components/powerbi-report.tsx
@@ -57,7 +57,7 @@ const useStyles = makeStyles()((theme) => ({
     [`& .${tabClasses.root}`]: {
       // Not done at theme level not to mess up with global navigation at the top
       minHeight: 56,
-      fontSize: "1.125rem",
+      fontSize: "1rem",
     },
     [`& .${tabScrollButtonClasses.root}`]: {
       alignItems: "center",
