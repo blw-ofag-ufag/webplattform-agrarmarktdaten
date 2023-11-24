5c36282ef8bd2d3db9a454ee0d825d2a043beefa
Beatriz Malveiro <mbeatrizmalveiro@gmail.com>
2023-11-24 11:29:15 +0100

    fix: subscribe location atom to next.js router

--- src/lib/use-locale.ts
+++ src/lib/use-locale.ts
@@ -1,13 +1,22 @@
 import { defaultLocale, Locale, locales } from "@/locales/locales";
 import { atom, useAtomValue } from "jotai";
 import { atomWithLocation } from "jotai-location";
+import { Router } from "next/router";
 
-const locationAtom = atomWithLocation();
+const locationAtom = atomWithLocation({
+  // Needed to make sure atom is synced with Next.js router
+  // https://github.com/pmndrs/jotai/issues/485#issuecomment-936215117
+  subscribe: (callback) => {
+    Router.events.on("routeChangeComplete", callback); // Subscribe to next router
+    return () => {
+      Router.events.off("routeChangeComplete", callback); // Unsubscribe to next router
+    };
+  },
+});
 
 export const localeAtom = atom<Locale>((get) => {
   const location = get(locationAtom);
   const locale = location.pathname?.split("/")[1];
-  console.log(locale);
   if (locale && locales.includes(locale as Locale)) return locale as Locale;
   return defaultLocale;
 });
