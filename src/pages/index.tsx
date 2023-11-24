5afde3b090dfcaf10de71b1c58b8ec8c8ed61329
Patrick Browne <pt.browne@gmail.com>
2023-11-24 12:01:22 +0100

    refactor: Extract DataPageCard and add a story

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Trans } from "@lingui/macro";
-import { Typography, Card, Button, useTheme } from "@mui/material";
+import { Typography, useTheme } from "@mui/material";
 import { Hero } from "@/components/hero";
 import { CardsGrid } from "@/components/homepage/grids";
 import { AppLayout, LayoutSections } from "@/components/layout";
@@ -7,48 +7,10 @@ import * as GQL from "@/graphql";
 import { client } from "@/graphql/api";
 import { TopBlogpostsTeaser } from "@/components/TopBlogpostsTeaser";
 import { s } from "@interactivethings/swiss-federal-ci";
-import { Download } from "@/icons/icons-jsx/control";
 import { GridContainer } from "@/components/Grid";
-import { makeStyles } from "@/components/style-utils";
-import CardActionButton from "@/components/CardActionButton";
-import Link from "next/link";
-
-const useStyles = makeStyles()(({ palette: c, spacing: s, shadows: e, breakpoints: b }) => ({
-  button: {
-    backgroundColor: c.cobalt[500],
-    fontWeight: 700,
-    width: "fit-content",
-    gridArea: "button",
-  },
-
-  card: {
-    boxShadow: e.lg,
-    borderRadius: s(2),
-    alignItems: "center",
-    gap: "1rem",
-    display: "grid",
-    gridTemplateColumns: "1fr max-content",
-    gridTemplateRows: "max-content max-content",
-    gridTemplateAreas: '"text icon" "button icon"',
-
-    [b.down("md")]: {
-      gridTemplateColumns: "1fr",
-      gridTemplateRows: "min-content max-content max-content",
-      gridTemplateAreas: '"icon" "text" "button"',
-    },
-  },
-
-  text: {
-    gridArea: "text",
-  },
-  icon: {
-    gridArea: "icon",
-    width: "fit-content",
-  },
-}));
+import { DataPageCard } from "../components/DataPageCard";
 
 export default function HomePage(props: GQL.HomePageQuery) {
-  const { classes } = useStyles();
   const { homePage, allMarketArticles, allFocusArticles, topBlogPosts } = props;
   const theme = useTheme();
   if (!homePage?.title || !homePage.lead) {
@@ -104,21 +66,7 @@ export default function HomePage(props: GQL.HomePageQuery) {
             <Typography variant="h1" component="h2">
               <Trans id="homepage.section.data">Data</Trans>
             </Typography>
-            <Card sx={{ p: s(8) }} className={classes.card}>
-              <Typography variant="body1" className={classes.text}>
-                <Trans id="homepage.section.data.content">
-                  Various data can be selected and downloaded via the data download, in particular
-                  price series, and in some cases also quantity and area data. Data are available
-                  from the year 2000 onwards.
-                </Trans>
-              </Typography>
-              <Button className={classes.button} component={Link} href="/data">
-                <Trans id="homepage.section.data.button">Learn More</Trans>
-              </Button>
-              <CardActionButton component={Link} href="/data" className={classes.icon}>
-                <Download width={32} height={32} />
-              </CardActionButton>
-            </Card>
+            <DataPageCard />
           </GridContainer>
         </LayoutSections>
       </AppLayout>
