b817dcec899184eb4f76d868f3da732e2dec0a2d
João Sobral <joao.tiago.sobral@gmail.com>
2023-11-27 21:39:20 +0000

    feat ⚡️: deal with the middleware slight of hand

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import redirects from "@/generated/redirects.json";
 
 const PUBLIC_FILE = /\.(.*)$/;
 
@@ -9,10 +10,15 @@ const productionUrls = [
   "www.agrarmarktdaten.ch",
 ];
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const basicAuth = req.headers.get("authorization");
   const url = req.nextUrl;
-  const { pathname } = url;
+  const { pathname, search } = url;
+
+  const redirect = redirectLocales(req);
+  if (redirect) {
+    return NextResponse.rewrite(new URL(`${redirect}${search}`, req.url));
+  }
 
   if (!productionUrls.some((prodUrl) => url.host.startsWith(prodUrl))) {
     return NextResponse.next();
@@ -39,4 +45,28 @@ export function middleware(req: NextRequest) {
   url.pathname = "/api/basicauth";
 
   return NextResponse.rewrite(url);
+}
+
+const DYNAMIC_ROUTE_REGEX = /\[\w+\d*\]/g;
+
+function redirectLocales(req: NextRequest) {
+  const match = redirects.find(({ alternates }) => {
+    return alternates.some((alt) => req.nextUrl.pathname.startsWith(alt));
+  });
+  if (match) {
+    // If it's a dynamic route [slug] we need to replace the dynamic bits
+    if (match.url.match(DYNAMIC_ROUTE_REGEX)) {
+      const [, ...incomingSegments] = req.nextUrl.pathname.split("/");
+      const [, ...outgoingSegments] = match.url.split("/");
+      const redirectUrl = outgoingSegments
+        .map((seg, i) => {
+          return seg.match(DYNAMIC_ROUTE_REGEX) ? incomingSegments[i] : seg;
+        })
+        .join("/");
+      return `/${redirectUrl}`;
+    }
+    //If we every use Catch-all Segments [...slug] (https://nextjs.org/docs/pages/building-your-application/routing/dynamic-routes#catch-all-segments) then that needs to be implemented
+    //If we every use Optional Catch-all Segments [[...slug]](https://nextjs.org/docs/pages/building-your-application/routing/dynamic-routes#optional-catch-all-segments) then that needs to be implemented
+  }
+  return match?.url;
 }
