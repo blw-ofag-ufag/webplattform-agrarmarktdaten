e2a8490166932dc2ba849168b10377a4b6255b72
Patrick Browne <pt.browne@gmail.com>
2023-12-01 16:02:16 +0100

    refactor: Put power bi report fullscreen button styles inside useStyles

--- src/components/powerbi-report.tsx
+++ src/components/powerbi-report.tsx
@@ -61,6 +61,12 @@ const useStyles = makeStyles()((theme) => ({
     },
   },
   fullscreenButton: {
+    position: "absolute",
+    bottom: "-3rem",
+    right: 0,
+    zIndex: 1000,
+    color: theme.palette.cobalt[500],
+    fontWeight: "bold",
     [theme.breakpoints.down("sm")]: {
       display: "none",
     },
@@ -220,13 +226,6 @@ export const PowerBIReport = (props: {
     <Box position="relative" ref={inViewRef}>
       <Button
         className={cx(inPlaceDialogClasses.hideWhenOpened, classes.fullscreenButton)}
-        sx={{
-          position: "absolute",
-          bottom: "-3rem",
-          right: 0,
-          color: "cobalt.500",
-          fontWeight: "bold",
-        }}
         startIcon={<IcExpand />}
         variant="inline"
         onClick={() => setFullscreen(true)}
