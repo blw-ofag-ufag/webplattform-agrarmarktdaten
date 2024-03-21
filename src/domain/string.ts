88969de7d66f43a8b1b1856fa7af48468e6bcfc1
João Sobral <joao.tiago.sobral@gmail.com>
2024-03-21 15:06:56 +0100

    feat ⚡️: transform accented chars

--- src/domain/string.ts
+++ src/domain/string.ts
@@ -5,6 +5,8 @@ export const replaceNumberSeparators = (string: string) =>
 export const slugify = (str: string) => {
   return str
     .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
     .replace(/[^\w\s-]/g, "")
     .replace(/\s+/g, "-")
     .replace(/--+/g, "-")
