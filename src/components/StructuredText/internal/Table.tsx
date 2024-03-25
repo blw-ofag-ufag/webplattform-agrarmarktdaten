a8b6372552315642f0ae1f2dc92332ae319d01d3
João Sobral <joao.tiago.sobral@gmail.com>
2024-03-25 15:59:16 +0000

    feat ⚡️: tables on structured text

---
+++ src/components/StructuredText/internal/Table.tsx
@@ -0,0 +1,88 @@
+import * as GQL from "@/graphql";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { makeStyles } from "@/components/style-utils";
+import { Typography } from "@mui/material";
+
+const useStyles = makeStyles()(({ palette: c, spacing: s }, _params) => ({
+  header: {
+    backgroundColor: c.cobalt[50],
+    border: "none",
+    borderBottom: `1px solid ${c.monochrome[300]}`,
+  },
+
+  headerCell: {
+    border: "none",
+    padding: s(4, 5),
+    textTransform: "uppercase",
+    fontWeight: 400,
+    color: c.cobalt[500],
+  },
+
+  row: {
+    borderBottom: `1px solid ${c.monochrome[300]}`,
+    "&:last-child": {
+      borderBottom: "none",
+    },
+  },
+
+  bodyCell: {
+    padding: s(4, 5),
+    border: "none",
+  },
+}));
+
+interface Props {
+  content: GQL.TableRecord["content"];
+}
+
+const JsonTable = (props: Props) => {
+  const { content } = props;
+  const { columns, data } = content;
+  const { classes } = useStyles();
+  return (
+    <Paper elevation={4}>
+      <Table aria-label="simple table">
+        <TableHead className={classes.header}>
+          <TableRow className={classes.row}>
+            {columns.length > 0 &&
+              columns.map((col: any, i: number) => {
+                return (
+                  <TableCell key={i} className={classes.headerCell}>
+                    <Typography variant="body3" color="textSecondary">
+                      {col}
+                    </Typography>
+                  </TableCell>
+                );
+              })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.length > 0 &&
+            data.map((row: Record<string, any>, i: number) => {
+              const cells = Object.values(row);
+              return (
+                <TableRow key={i} className={classes.row}>
+                  {cells.map((cell, i) => {
+                    return (
+                      <TableCell key={i} className={classes.bodyCell}>
+                        <Typography variant="body2" color="textSecondary">
+                          {cell as any}
+                        </Typography>
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+};
+
+export default JsonTable;
