7488dcc6ba1ec5dcd14a0959b6105fda5ebcbe9e
Patrick Browne <pt.browne@gmail.com>
2024-03-26 08:56:13 +0100

    fix: Disable replacing space by small space since iOS does not support lookbehinds
    https://stackoverflow.com/questions/51568821/works-in-chrome-but-breaks-in-safari-invalid-regular-expression-invalid-group

--- src/domain/string.ts
+++ src/domain/string.ts
@@ -1,6 +1,8 @@
-const BIGNUMBER_SPACE_REGEX = /(?<=\b\d{1,3})\s(?=\d{1,3}\b)/g;
-export const replaceNumberSeparators = (string: string) =>
-  string.replace(BIGNUMBER_SPACE_REGEX, "\u00A0");
+// TODO Reactivate when we have a Regex that works on Safari iOS.
+// See also the spec file where the test is skipped.
+// const BIGNUMBER_SPACE_REGEX = /(?<=\b\d{1,3})\s(?=\d{1,3}\b)/g;
+// export const replaceNumberSeparators = (string: string) => string.replace(BIGNUMBER_SPACE_REGEX, "\u00A0");
+export const replaceNumberSeparators = (string: string) => string;
 
 export const slugify = (str: string) => {
   return str
