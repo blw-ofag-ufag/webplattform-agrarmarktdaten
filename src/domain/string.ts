7bd127793174abb86789dbc6546d539d21acace7
João Sobral <joao.tiago.sobral@gmail.com>
2024-03-27 14:47:03 +0000

    feat ⚡️: replace leadCard with cardSummary

--- src/domain/string.ts
+++ src/domain/string.ts
@@ -1,9 +1,3 @@
-// TODO Reactivate when we have a Regex that works on Safari iOS.
-// See also the spec file where the test is skipped.
-// const BIGNUMBER_SPACE_REGEX = /(?<=\b\d{1,3})\s(?=\d{1,3}\b)/g;
-// export const replaceNumberSeparators = (string: string) => string.replace(BIGNUMBER_SPACE_REGEX, "\u00A0");
-export const replaceNumberSeparators = (string: string) => string;
-
 export const slugify = (str: string) => {
   return str
     .toLowerCase()
